ecf59d6501d47e8533deeee510a74b3d91dc42c8
Tugba <zeyneptubayucesar@gmail.com>
2026-05-05 22:59:19 +0300

    Update marketRoutes.js

--- routes/marketRoutes.js
+++ routes/marketRoutes.js
@@ -8,12 +8,15 @@ router.use(isMarket);
 
 router.get("/dashboard", marketController.dashboard);
 router.get("/profile", marketController.showProfile);
+
 router.post("/profile", marketController.updateProfile);
 
 router.get("/product/add", marketController.showAddProduct);
+
 router.post("/product/add", upload.single("image"), marketController.addProduct);
 
 router.get("/product/edit/:id", marketController.showEditProduct);
+
 router.post("/product/edit/:id", upload.single("image"), marketController.editProduct);
 
 router.post("/product/delete/:id", marketController.deleteProduct);
