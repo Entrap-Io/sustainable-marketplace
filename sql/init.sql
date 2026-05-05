f2cce01ca57c1652031f3ef74f98ac435b6e75e0
Tugba <zeyneptubayucesar@gmail.com>
2026-05-05 21:13:24 +0300

    Update init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -38,24 +38,24 @@ CREATE TABLE IF NOT EXISTS cart_items (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Seed Data (Password is '1234' for all test users)
--- Market User: Tok Market
+-- Market User 1: Tok Market
 INSERT INTO users (email, password, role, name, city, district, is_verified) VALUES 
 ('tok-market@gmail.com', '$2b$10$dHBl9BdDBs7Y1wE7jP89L.oYi8Q8kH/JwxEl2sP8n7noIiObQs.DC', 'market', 'Tok Market', 'Ankara', 'Bilkent', TRUE);
 
--- Market User: Bim Market
+-- Market User 2: Bim Market
 INSERT INTO users (email, password, role, name, city, district, is_verified) VALUES 
 ('bim-market@gmail.com', '$2b$10$dHBl9BdDBs7Y1wE7jP89L.oYi8Q8kH/JwxEl2sP8n7noIiObQs.DC', 'market', 'Bim Market', 'Ankara', 'Cankaya', TRUE);
 
--- Market User: File Market
+-- Market User 3: File Market
 INSERT INTO users (email, password, role, name, city, district, is_verified) VALUES 
 ('file-market@gmail.com', '$2b$10$dHBl9BdDBs7Y1wE7jP89L.oYi8Q8kH/JwxEl2sP8n7noIiObQs.DC', 'market', 'File Market', 'Ankara', 'Cankaya', TRUE);
 
--- Market User: Target Market
+-- Market User 4: Target Market
 INSERT INTO users (email, password, role, name, city, district, is_verified) VALUES 
 ('target-market@gmail.com', '$2b$10$dHBl9BdDBs7Y1wE7jP89L.oYi8Q8kH/JwxEl2sP8n7noIiObQs.DC', 'market', 'Target Market', 'Ankara', 'Bilkent', TRUE);
 
 
--- Consumer User: John Doe
+-- Consumer 1: John Doe
 INSERT INTO users (email, password, role, name, city, district, is_verified) VALUES 
 ('consumer@gmail.com', '$2b$10$dHBl9BdDBs7Y1wE7jP89L.oYi8Q8kH/JwxEl2sP8n7noIiObQs.DC', 'consumer', 'John Doe', 'Ankara', 'Bilkent', TRUE);
 
@@ -79,28 +79,28 @@ INSERT INTO users (email, password, role, name, city, district, is_verified) VAL
 INSERT INTO users (email, password, role, name, city, district, is_verified) VALUES 
 ('kyra@gmail.com', '$2b$10$dHBl9BdDBs7Y1wE7jP89L.oYi8Q8kH/JwxEl2sP8n7noIiObQs.DC', 'consumer', 'Kyra Hopkins', 'Ankara', 'Cankaya', TRUE);
 
--- Products (market user has id=1) Tok
+-- Products for (market user has id=1) Tok
 INSERT INTO products (title, stock, normal_price, discounted_price, expiration_date, image_filename, market_id) VALUES 
 ('Toblerone 100gr', 25, 200.00, 120.00, DATE_ADD(CURDATE(), INTERVAL 1 MONTH), 'toblerone.jpg', 1),
 ('Magnum Ice Cream', 50, 45.00, 30.00, DATE_ADD(CURDATE(), INTERVAL 2 DAY), 'magnum.jpg', 1),
 ('Magnolia Cake', 10, 150.00, 80.00, DATE_ADD(CURDATE(), INTERVAL 5 DAY), 'magnolia.jpg', 1),
 ('Nutmeg Spice', 100, 50.00, 20.00, DATE_ADD(CURDATE(), INTERVAL 1 YEAR), 'nutmeg.jpg', 1);
 
--- Products (market user has id=2) Bim
+-- Products for (market user has id=2) Bim
 INSERT INTO products (title, stock, normal_price, discounted_price, expiration_date, image_filename, market_id) VALUES 
 ('Milk 1L', 5, 30.00, 10.00, DATE_SUB(CURDATE(), INTERVAL 2 DAY), 'milk.jpg', 2),
 ('Banana 1kg', 40, 60.00, 35.00, DATE_SUB(CURDATE(), INTERVAL 3 DAY), 'banana.jpg', 2),
 ('Apple 1kg', 60, 50.00, 30.00, DATE_SUB(CURDATE(), INTERVAL 15 DAY), 'apple.jpg', 2),
 ('Cheese', 15, 120.00, 85.00, DATE_SUB(CURDATE(), INTERVAL 2 MONTH), 'cheese.jpg', 2);
 
--- Products (market user has id=3) File 
+-- Products for (market user has id=3) File 
 INSERT INTO products (title, stock, normal_price, discounted_price, expiration_date, image_filename, market_id) VALUES 
 ('Tomato 1kg', 30, 40.00, 20.00, DATE_SUB(CURDATE(), INTERVAL 12 DAY), 'tomato.jpg', 3),
 ('Cucumber 1kg', 70, 35.00, 18.00, DATE_SUB(CURDATE(), INTERVAL 8 DAY), 'cucumber.jpg', 3),
 ('Orange Juice 1L', 45, 80.00, 55.00, DATE_SUB(CURDATE(), INTERVAL 8 DAY), 'orangejuice.jpg', 3),
 ('Eggs (12 Pack)', 50, 110.00,75.00, DATE_SUB(CURDATE(), INTERVAL 1 MONTH), 'eggs.jpg', 3);
 
--- Products (market user has id=4) Target 
+-- Products for (market user has id=4) Target 
 INSERT INTO products (title, stock, normal_price, discounted_price, expiration_date, image_filename, market_id) VALUES 
 ('Nutella', 30, 200.00, 120.00, DATE_SUB(CURDATE(), INTERVAL 25 DAY), 'nutella.jpg', 4),
 ('Toast Bread', 60, 65.00, 40.00, DATE_SUB(CURDATE(), INTERVAL 1 MONTH), 'toast.jpg', 4),
