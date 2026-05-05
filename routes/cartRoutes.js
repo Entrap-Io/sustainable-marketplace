1fd58779d253c2f4213492d18ec9a280d4d382e7
Tugba <zeyneptubayucesar@gmail.com>
2026-05-05 21:11:38 +0300

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -6,9 +6,13 @@ const router = express.Router();
 router.use(isConsumer);
 
 router.get("/", cartController.viewCart);
+
 router.get("/count", cartController.getCartCount);
+
 router.post("/add", cartController.addToCart);
+
 router.post("/update", cartController.updateCart);
+
 router.post("/purchase", cartController.purchase);
 
 export default router;
